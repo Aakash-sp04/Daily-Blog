1c3fccee9f11c75b8357cccbda297fdd0ae78b46
AAKASH PACHCHIGAR <84685121+Aakash-sp04@users.noreply.github.com>
2023-06-10 20:52:26 +0530

    Update app.js
    change callbackURL of google api

--- backend/app.js
+++ backend/app.js
@@ -107,7 +107,7 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://daily-blog-we0e.onrender.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
